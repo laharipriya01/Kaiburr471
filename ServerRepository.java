f4bb74891a85684c174ba45bbd031e0ab75e484c
lalala2001 <71052619+lalala2001@users.noreply.github.com>
2022-04-12 22:32:53 +0530

    Add files via upload

---
+++ ServerRepository.java
@@ -0,0 +1,8 @@
+package main;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ServerRepository extends MongoRepository<Server, String> {
+	public List<Server> findByName(String name);
+}
